04c403046f344dbd677783225db477562cc4f820
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-02-28 16:02:53 -0500

    Took the code and refactored into separate javascript file.

---
+++ scripts.js
@@ -0,0 +1,42 @@
+  // Load the Visualization API and the piechart package.
+  //credit Google with code sample
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+      	
+      	var alltheDataArray = []; 
+      	
+      	var Obs = alltheData.observations;
+      	
+      	for(var i=0; i<Obs.length; i++){
+      		
+      		var Item = [];
+      		
+      		Item.push(Obs[i].date);
+      		Item.push(Number(Obs[i].value));
+      		alltheDataArray.push(Item);
+      		
+      	}
+			//console.log(alltheDataArray); 
+			
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows(alltheDataArray);
+
+        // Set chart options
+        var options = {'title':'Pizza Eating',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
